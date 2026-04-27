71b3368fb5303494bf6b391b621a8f6e3c7a715f
shubhampawar-ethara <shubham.pawar@ethara.ai>
2026-04-27 14:01:49 +0530

    Commit 0: Stub all functions, remove .github/workflows

--- yahoofinancials/cache.py
+++ yahoofinancials/cache.py
@@ -24,14 +24,14 @@ class _TzCacheDummy:
     """Dummy cache to use if tz cache is disabled"""
 
     def lookup(self, tkr):
-        return None
+        pass
 
     def store(self, tkr, tz):
         pass
 
     @property
     def tz_db(self):
-        return None
+        pass
 
 
 class _TzCacheManager:
@@ -39,14 +39,11 @@ class _TzCacheManager:
 
     @classmethod
     def get_tz_cache(cls):
-        if cls._tz_cache is None:
-            with _cache_init_lock:
-                cls._initialise()
-        return cls._tz_cache
+        pass
 
     @classmethod
     def _initialise(cls, cache_dir=None):
-        cls._tz_cache = _TzCache()
+        pass
 
 
 class _TzDBManager:
@@ -55,52 +52,23 @@ class _TzDBManager:
 
     @classmethod
     def get_database(cls):
-        if cls._db is None:
-            cls._initialise()
-        return cls._db
+        pass
 
     @classmethod
     def close_db(cls):
-        if cls._db is not None:
-            try:
-                cls._db.close()
-            except Exception:
-                # Must discard exceptions because Python trying to quit.
-                pass
+        pass
 
     @classmethod
     def _initialise(cls, cache_dir=None):
-        if cache_dir is not None:
-            cls._cache_dir = cache_dir
-
-        if not _os.path.isdir(cls._cache_dir):
-            try:
-                _os.makedirs(cls._cache_dir)
-            except OSError as err:
-                raise _TzCacheException(
-                    f"yahoofinancials: Error creating TzCache folder: '{cls._cache_dir}' reason: {err}")
-        elif not (_os.access(cls._cache_dir, _os.R_OK) and _os.access(cls._cache_dir, _os.W_OK)):
-            raise _TzCacheException(f"yahoofinancials: Cannot read and write in TzCache folder: '{cls._cache_dir}'")
-
-        cls._db = _peewee.SqliteDatabase(
-            _os.path.join(cls._cache_dir, 'tkr-tz.db'),
-            pragmas={'journal_mode': 'wal', 'cache_size': -64}
-        )
-
-        old_cache_file_path = _os.path.join(cls._cache_dir, "tkr-tz.csv")
-        if _os.path.isfile(old_cache_file_path):
-            _os.remove(old_cache_file_path)
+        pass
 
     @classmethod
     def set_location(cls, new_cache_dir):
-        if cls._db is not None:
-            cls._db.close()
-            cls._db = None
-        cls._cache_dir = new_cache_dir
+        pass
 
     @classmethod
     def get_location(cls):
-        return cls._cache_dir
+        pass
 
 
 # close DB when Python exists
@@ -125,81 +93,20 @@ class _TzCache:
         self.dummy = False
 
     def get_db(self):
-        if self.db is not None:
-            return self.db
-
-        try:
-            self.db = _TzDBManager.get_database()
-        except _TzCacheException as err:
-            logging.info(f"yahoofinancials: Failed to create TzCache, reason: {err}. "
-                         "TzCache will not be used. "
-                         "Tip: You can direct cache to use a different location with 'set_tz_cache_location("
-                         "mylocation)'")
-            self.dummy = True
-            return None
-        return self.db
+        pass
 
     def initialise(self):
-        if self.initialised != -1:
-            return
-
-        db = self.get_db()
-        if db is None:
-            self.initialised = 0  # failure
-            return
-
-        db.connect()
-        tz_db_proxy.initialize(db)
-        db.create_tables([_KV])
-        self.initialised = 1  # success
+        pass
 
     def lookup(self, key):
-        if self.dummy:
-            return None
-
-        if self.initialised == -1:
-            self.initialise()
-
-        if self.initialised == 0:  # failure
-            return None
-
-        try:
-            return _KV.get(_KV.key == key).value
-        except _KV.DoesNotExist:
-            return None
+        pass
 
     def store(self, key, value):
-        if self.dummy:
-            return
-
-        if self.initialised == -1:
-            self.initialise()
-
-        if self.initialised == 0:  # failure
-            return
-
-        db = self.get_db()
-        if db is None:
-            return
-        try:
-            if value is None:
-                q = _KV.delete().where(_KV.key == key)
-                q.execute()
-                return
-            with db.atomic():
-                _KV.insert(key=key, value=value).execute()
-        except _peewee.IntegrityError:
-            # Integrity error means the key already exists. Try updating the key.
-            old_value = self.lookup(key)
-            if old_value != value:
-                logging.debug(f"yahoofinancials: Value for key {key} changed from {old_value} to {value}.")
-                with db.atomic():
-                    q = _KV.update(value=value).where(_KV.key == key)
-                    q.execute()
+        pass
 
 
 def get_tz_cache():
-    return _TzCacheManager.get_tz_cache()
+    pass
 
 
 def set_tz_cache_location(cache_dir: str):
@@ -210,7 +117,7 @@ def set_tz_cache_location(cache_dir: str):
     :param cache_dir: Path to use for caches
     :return: None
     """
-    _TzDBManager.set_location(cache_dir)
+    pass
 
 
 # --------------
@@ -225,14 +132,14 @@ class _CookieCacheDummy:
     """Dummy cache to use if Cookie cache is disabled"""
 
     def lookup(self, tkr):
-        return None
+        pass
 
     def store(self, tkr, Cookie):
         pass
 
     @property
     def Cookie_db(self):
-        return None
+        pass
 
 
 class _CookieCacheManager:
@@ -240,14 +147,11 @@ class _CookieCacheManager:
 
     @classmethod
     def get_cookie_cache(cls):
-        if cls._Cookie_cache is None:
-            with _cache_init_lock:
-                cls._initialise()
-        return cls._Cookie_cache
+        pass
 
     @classmethod
     def _initialise(cls, cache_dir=None):
-        cls._Cookie_cache = _CookieCache()
+        pass
 
 
 class _CookieDBManager:
@@ -256,49 +160,23 @@ class _CookieDBManager:
 
     @classmethod
     def get_database(cls):
-        if cls._db is None:
-            cls._initialise()
-        return cls._db
+        pass
 
     @classmethod
     def close_db(cls):
-        if cls._db is not None:
-            try:
-                cls._db.close()
-            except Exception:
-                # Must discard exceptions because Python trying to quit.
-                pass
+        pass
 
     @classmethod
     def _initialise(cls, cache_dir=None):
-        if cache_dir is not None:
-            cls._cache_dir = cache_dir
-
-        if not _os.path.isdir(cls._cache_dir):
-            try:
-                _os.makedirs(cls._cache_dir)
-            except OSError as err:
-                raise _CookieCacheException(
-                    f"yahoofinancials: Error creating CookieCache folder: '{cls._cache_dir}' reason: {err}")
-        elif not (_os.access(cls._cache_dir, _os.R_OK) and _os.access(cls._cache_dir, _os.W_OK)):
-            raise _CookieCacheException(
-                f"yahoofinancials: Cannot read and write in CookieCache folder: '{cls._cache_dir}'")
-
-        cls._db = _peewee.SqliteDatabase(
-            _os.path.join(cls._cache_dir, 'cookies.db'),
-            pragmas={'journal_mode': 'wal', 'cache_size': -64}
-        )
+        pass
 
     @classmethod
     def set_location(cls, new_cache_dir):
-        if cls._db is not None:
-            cls._db.close()
-            cls._db = None
-        cls._cache_dir = new_cache_dir
+        pass
 
     @classmethod
     def get_location(cls):
-        return cls._cache_dir
+        pass
 
 
 # close DB when Python exists
@@ -326,74 +204,16 @@ class _CookieCache:
         self.dummy = False
 
     def get_db(self):
-        if self.db is not None:
-            return self.db
-
-        try:
-            self.db = _CookieDBManager.get_database()
-        except _CookieCacheException as err:
-            logging.info(f"yahoofinancials: Failed to create CookieCache, reason: {err}. "
-                         "CookieCache will not be used. "
-                         "Tip: You can direct cache to use a different location with 'set_tz_cache_location("
-                         "mylocation)'")
-            self.dummy = True
-            return None
-        return self.db
+        pass
 
     def initialise(self):
-        if self.initialised != -1:
-            return
-        db = self.get_db()
-        if db is None:
-            self.initialised = 0  # failure
-            return
-        i = 0
-        while i < 250:
-            try:
-                db.connect()
-                Cookie_db_proxy.initialize(db)
-                db.create_tables([_CookieSchema])
-                self.initialised = 1  # success
-                return
-            except _peewee.OperationalError:
-                time.sleep(random.randint(3, 25))
-                continue
-        self.initialised = 0  # failure
+        pass
 
     def lookup(self, strategy):
-        if self.dummy:
-            return None
-        if self.initialised == -1:
-            self.initialise()
-        if self.initialised == 0:  # failure
-            return None
-        try:
-            data = _CookieSchema.get(_CookieSchema.strategy == strategy)
-            cookie = _pkl.loads(data.cookie_bytes)
-            return {'cookie': cookie, 'age': _datetime.datetime.now() - data.fetch_date}
-        except _CookieSchema.DoesNotExist:
-            return None
+        pass
 
     def store(self, strategy, cookie):
-        if self.dummy:
-            return
-        if self.initialised == -1:
-            self.initialise()
-        if self.initialised == 0:  # failure
-            return
-        db = self.get_db()
-        if db is None:
-            return
-        try:
-            q = _CookieSchema.delete().where(_CookieSchema.strategy == strategy)
-            q.execute()
-            if cookie is None:
-                return
-            with db.atomic():
-                cookie_pkl = _pkl.dumps(cookie, _pkl.HIGHEST_PROTOCOL)
-                _CookieSchema.insert(strategy=strategy, cookie_bytes=cookie_pkl).execute()
-        except _peewee.IntegrityError:
-            raise
+        pass
             # # Integrity error means the strategy already exists. Try updating the strategy.
             # old_value = self.lookup(strategy)
             # if old_value != cookie:
@@ -404,4 +224,4 @@ class _CookieCache:
 
 
 def get_cookie_cache():
-    return _CookieCacheManager.get_cookie_cache()
+    pass
--- yahoofinancials/yf.py
+++ yahoofinancials/yf.py
@@ -74,311 +74,200 @@ class YahooFinancials(YahooFinanceData):
 
     # Private method that handles financial statement extraction
     def _run_financial_stmt(self, statement_type, report_num, frequency, reformat):
-        hist_obj = {"interval": frequency}
-        report_name = self.YAHOO_FINANCIAL_TYPES[statement_type][report_num]
-        if reformat:
-            raw_data = self.get_stock_data(statement_type, report_name=report_name, hist_obj=hist_obj)
-            data = self.get_reformatted_stmt_data(raw_data)
-        else:
-            data = self.get_stock_data(statement_type, report_name=report_name, hist_obj=hist_obj)
-        return data
+        pass
 
     # Public Method for the user to get financial statement data
     def get_financial_stmts(self, frequency, statement_type, reformat=True):
-        report_num = self.get_report_type(frequency)
-        if isinstance(statement_type, str):
-            data = self._run_financial_stmt(statement_type, report_num, frequency, reformat)
-        else:
-            data = {}
-            for stmt_type in statement_type:
-                re_data = self._run_financial_stmt(stmt_type, report_num, frequency, reformat)
-                data.update(re_data)
-        return data
+        pass
 
     # Public Method for the user to get stock price data
     def get_stock_price_data(self, reformat=True):
-        if reformat:
-            return self.get_clean_data(self.get_stock_tech_data('price'), 'price')
-        else:
-            return self.get_stock_tech_data('price')
+        pass
 
     # Public Method for the user to return key-statistics data
     def get_key_statistics_data(self, reformat=True):
-        if reformat:
-            return self.get_clean_data(self.get_stock_tech_data('defaultKeyStatistics'), 'defaultKeyStatistics')
-        else:
-            return self.get_stock_tech_data('defaultKeyStatistics')
+        pass
 
     # Public Method for the user to get company profile data
     def get_stock_profile_data(self, reformat=True):
-        if reformat:
-            return self.get_clean_data(
-                self.get_stock_data(statement_type='profile', tech_type='assetProfile', report_name='assetProfile'),
-                'earnings')
-        else:
-            return self.get_stock_data(statement_type='profile', tech_type='assetProfile', report_name='assetProfile')
+        pass
 
     # Public Method for the user to get stock earnings data
     def get_stock_earnings_data(self):
-        return self.get_stock_tech_data('earnings')
+        pass
 
     # Public Method for the user to return financial data
     def get_financial_data(self, reformat=True):
-        if reformat:
-            return self.get_clean_data(self.get_stock_data(statement_type='keystats', tech_type='financialData'),
-                                       'financialData')
-        else:
-            return self.get_stock_data(statement_type='keystats', tech_type='financialData')
+        pass
 
     # Public Method for the user to get stock summary data
     def get_summary_data(self, reformat=True):
-        if reformat:
-            return self.get_clean_data(self.get_stock_tech_data('summaryDetail'), 'summaryDetail')
-        else:
-            return self.get_stock_tech_data('summaryDetail')
+        pass
 
     # Public Method for the user to get the yahoo summary url
     def get_stock_summary_url(self):
-        if isinstance(self.ticker, str):
-            return self._BASE_YAHOO_URL + self.ticker
-        return {t: self._BASE_YAHOO_URL + t for t in self.ticker}
+        pass
 
     # Public Method for the user to get stock quote data
     def get_stock_quote_type_data(self):
-        return self.get_stock_tech_data('quoteType')
+        pass
 
     # Public Method for the user to get stock quote data
     def get_esg_score_data(self):
-        return self.get_stock_tech_data('esgScores')
+        pass
 
     def _get_analytic_data(self, tech_type):
-        return self.get_stock_data(statement_type='analytic', tech_type=tech_type)
+        pass
 
     # Public Method for user to get historical price data with
     def get_historical_price_data(self, start_date, end_date, time_interval):
-        interval_code = self.get_time_code(time_interval)
-        start = self.format_date(start_date)
-        end = self.format_date(end_date)
-        hist_obj = {'start': start, 'end': end, 'interval': interval_code}
-        return self.get_stock_data('history', hist_obj=hist_obj)
+        pass
 
     # Private Method for Functions needing stock_price_data
     def _stock_price_data(self, data_field):
-        price_data = self.get_stock_price_data()
-        if isinstance(self.ticker, str):
-            if price_data[self.ticker] is None:
-                return None
-            return price_data[self.ticker].get(data_field)
-        else:
-            ret_obj = {}
-            for tick in self.ticker:
-                if price_data[tick] is None:
-                    ret_obj.update({tick: None})
-                else:
-                    ret_obj.update({tick: price_data[tick].get(data_field)})
-            return ret_obj
+        pass
 
     # Private Method for Functions needing stock_price_data
     def _stock_summary_data(self, data_field):
-        sum_data = self.get_summary_data()
-        if isinstance(self.ticker, str):
-            if sum_data[self.ticker] is None:
-                return None
-            return sum_data[self.ticker].get(data_field, None)
-        else:
-            ret_obj = {}
-            for tick in self.ticker:
-                if sum_data[tick] is None:
-                    ret_obj.update({tick: None})
-                else:
-                    ret_obj.update({tick: sum_data[tick].get(data_field, None)})
-            return ret_obj
+        pass
 
     # Private Method for Functions needing financial statement data
     def _financial_statement_data(self, stmt_type, stmt_code, field_name, freq):
-        re_data = self.get_financial_stmts(freq, stmt_type)[stmt_code]
-        if isinstance(self.ticker, str):
-            try:
-                date_key = re_data[self.ticker][0].keys()[0]
-            except (IndexError, AttributeError, TypeError):
-                date_key = list(re_data[self.ticker][0])[0]
-            data = re_data[self.ticker][0][date_key].get(field_name)
-        else:
-            data = {}
-            for tick in self.ticker:
-                try:
-                    date_key = re_data[tick][0].keys()[0]
-                except:
-                    try:
-                        date_key = list(re_data[tick][0].keys())[0]
-                    except:
-                        date_key = None
-                if date_key is not None:
-                    sub_data = re_data[tick][0][date_key][field_name]
-                    data.update({tick: sub_data})
-                else:
-                    data.update({tick: None})
-        return data
+        pass
 
     # Public method to get daily dividend data
     def get_daily_dividend_data(self, start_date, end_date):
-        start = self.format_date(start_date)
-        end = self.format_date(end_date)
-        return self.get_stock_dividend_data(start, end, 'daily')
+        pass
 
     # Public Price Data Methods
     def get_current_price(self):
-        return self._stock_price_data('regularMarketPrice')
+        pass
 
     def get_current_change(self):
-        return self._stock_price_data('regularMarketChange')
+        pass
 
     def get_current_percent_change(self):
-        return self._stock_price_data('regularMarketChangePercent')
+        pass
 
     def get_current_volume(self):
-        return self._stock_price_data('regularMarketVolume')
+        pass
 
     def get_prev_close_price(self):
-        return self._stock_price_data('regularMarketPreviousClose')
+        pass
 
     def get_open_price(self):
-        return self._stock_price_data('regularMarketOpen')
+        pass
 
     def get_ten_day_avg_daily_volume(self):
-        return self._stock_summary_data('averageDailyVolume10Day')
+        pass
 
     def get_stock_exchange(self):
-        return self._stock_price_data('exchangeName')
+        pass
 
     def get_market_cap(self):
-        return self._stock_price_data('marketCap')
+        pass
 
     def get_daily_low(self):
-        return self._stock_price_data('regularMarketDayLow')
+        pass
 
     def get_daily_high(self):
-        return self._stock_price_data('regularMarketDayHigh')
+        pass
 
     def get_currency(self):
-        return self._stock_price_data('currency')
+        pass
 
     # Public Summary Data Methods
     def get_yearly_high(self):
-        return self._stock_summary_data('fiftyTwoWeekHigh')
+        pass
 
     def get_yearly_low(self):
-        return self._stock_summary_data('fiftyTwoWeekLow')
+        pass
 
     def get_dividend_yield(self):
-        return self._stock_summary_data('dividendYield')
+        pass
 
     def get_annual_avg_div_yield(self):
-        return self._stock_summary_data('trailingAnnualDividendYield')
+        pass
 
     def get_five_yr_avg_div_yield(self):
-        return self._stock_summary_data('fiveYearAvgDividendYield')
+        pass
 
     def get_dividend_rate(self):
-        return self._stock_summary_data('dividendRate')
+        pass
 
     def get_annual_avg_div_rate(self):
-        return self._stock_summary_data('trailingAnnualDividendRate')
+        pass
 
     def get_50day_moving_avg(self):
-        return self._stock_summary_data('fiftyDayAverage')
+        pass
 
     def get_200day_moving_avg(self):
-        return self._stock_summary_data('twoHundredDayAverage')
+        pass
 
     def get_beta(self):
-        return self._stock_summary_data('beta')
+        pass
 
     def get_payout_ratio(self):
-        return self._stock_summary_data('payoutRatio')
+        pass
 
     def get_pe_ratio(self):
-        return self._stock_summary_data('trailingPE')
+        pass
 
     def get_price_to_sales(self):
-        return self._stock_summary_data('priceToSalesTrailing12Months')
+        pass
 
     def get_exdividend_date(self):
-        return self._stock_summary_data('exDividendDate')
+        pass
 
     # Financial Statement Data Methods
     def get_book_value(self):
-        return self._financial_statement_data('balance', 'balanceSheetHistoryQuarterly',
-                                              'totalStockholderEquity', 'quarterly')
+        pass
 
     def get_ebit(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'ebit', 'annual')
+        pass
 
     def get_net_income(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'netIncome', 'annual')
+        pass
 
     def get_interest_expense(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'interestExpense', 'annual')
+        pass
 
     def get_operating_income(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'netIncomeContinuousOperations',
-                                              'annual')
+        pass
 
     def get_total_operating_expense(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'totalOperatingExpenses', 'annual')
+        pass
 
     def get_total_revenue(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'totalRevenue', 'annual')
+        pass
 
     def get_cost_of_revenue(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'costOfRevenue', 'annual')
+        pass
 
     def get_income_before_tax(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'incomeBeforeTax', 'annual')
+        pass
 
     def get_income_tax_expense(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'incomeTaxExpense', 'annual')
+        pass
 
     def get_gross_profit(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'grossProfit', 'annual')
+        pass
 
     def get_net_income_from_continuing_ops(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory',
-                                              'netIncomeFromContinuingOps', 'annual')
+        pass
 
     def get_research_and_development(self):
-        return self._financial_statement_data('income', 'incomeStatementHistory', 'researchDevelopment', 'annual')
+        pass
 
     def get_recommendations(self):
-        return self._get_analytic_data("recommendations")
+        pass
 
     def get_insights(self):
-        return self._get_analytic_data("insights")
+        pass
 
     # Calculated Financial Methods
     def get_earnings_per_share(self):
-        price_data = self.get_current_price()
-        pe_ratio = self.get_pe_ratio()
-        if isinstance(self.ticker, str):
-            return eps(price_data, pe_ratio)
-        else:
-            ret_obj = {}
-            for tick in self.ticker:
-                re_val = eps(price_data[tick], pe_ratio[tick])
-                ret_obj.update({tick: re_val})
-            return ret_obj
+        pass
 
     def get_num_shares_outstanding(self, price_type='current'):
-        today_low = self._stock_summary_data('dayHigh')
-        today_high = self._stock_summary_data('dayLow')
-        cur_market_cap = self._stock_summary_data('marketCap')
-        current = self.get_current_price()
-        if isinstance(self.ticker, str):
-            return num_shares_outstanding(cur_market_cap, today_low, today_high, price_type, current)
-        else:
-            ret_obj = {}
-            for tick in self.ticker:
-                re_data = num_shares_outstanding(cur_market_cap[tick], today_low[tick],
-                                                 today_high[tick], price_type, current[tick])
-                ret_obj.update({tick: re_data})
-            return ret_obj
+        pass
